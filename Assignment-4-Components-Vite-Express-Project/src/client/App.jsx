4727368c2bee100f8459a0aa812aa49c8aff638c
ryanveith <ryanleeveith@gmail.com>
2026-09-18 09:58:32 -0400

    app is somewhat of a match, need to redo server and the actual non-login page. Also probably switch stuff to usestate we will see

--- Assignment-4-Components-Vite-Express-Project/src/client/App.jsx
+++ Assignment-4-Components-Vite-Express-Project/src/client/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Fragment } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Fragment>
+      <title>Scoredisplay Login - CS4241 Assignment 2</title>
+      <meta charset='utf-8' />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous" />
+      <meta name="description" content="The login page for a simple database that lets you store highscores for games you play." />
+      <main>
+        <form class="pure-form" id="login-form">
+          <h1> Scoredisplay Login Page </h1>
+          <section class="pure-g" id="layout">
+            <label for="username"> Select Login/Sign Up: </label>
+            <select class="pure-u-1 pure-u-md-1-3" id='option'>
+              <option>Login</option>
+              <option>Create Account</option>
+            </select>
+            <label for="username"> Enter your Username: </label>
+            <input class="pure-u-1 pure-u-md-1-3" type='text' id='username' value=''placeholder='enter your username'/>
+            <label for="username"> Enter your Password: </label>
+            <input class="pure-u-1 pure-u-md-1-3" type='password' id='password' value='' placeholder='enter your password here'/>
+            <div class="pure-u-1" id='error'></div>
+            <button class="pure-button pure-button-primary pure-u-1" type="button" onclick="login()" >Log In</button>
+          </section>
+        </form>
+      </main>
+    </Fragment>
   );
 }
 
